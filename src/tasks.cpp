e5dbc64e22784b8f07df3882b3705d145d16940f
Ariel Young <arielyoung103@gmail.com>
2019-11-27 14:06:09 -0800

    Moved intake position constants to constants.h

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -21,15 +21,15 @@ void intake_task_fn(void* timeout_ptr) {
     motor2.move(0);
 }
 
-const double liftkP = 1.0; /// TODO: Replace these values with actual tuned PID loop values
+const double liftkP = 1.0; // TODO: Replace these values with actual tuned PID loop values
 const double liftkI = 0.001;
 const double liftkD = 0.02;
 
-/// TODO: Consider just moving this into auto code? Having it here seems really dumb
+// TODO: Consider just moving this into auto code? Having it here seems really dumb
 void move_intake_task_fn(void * thisTaskPtr) {
     auto * thisTask = (pros::Task *) thisTaskPtr;
     auto intakePosController = okapi::AsyncControllerFactory::posPID(robot::INTAKE_POSITION_MOTOR_PORT, liftkP, liftkI, liftkD);
-    int targetLoc = -600; /// TODO: Change this to the actual target location of intakeDown
+    int targetLoc = -600; // TODO: Change this to the actual target location of intakeDown
 
     /// Assume that each task notification is a request to toggle up/down
     while (thisTask->notify_take(true, TIMEOUT_MAX)) {
