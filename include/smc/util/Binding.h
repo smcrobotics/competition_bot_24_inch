1aa3c6e8dc89e42adffdeabb308d91095dc1a7e4
Ariel Young <arielyoung103@gmail.com>
2019-11-28 19:39:42 -0800

    Added Binding class for (maybe?) more efficient binding code

--- include/smc/util/Binding.h
+++ include/smc/util/Binding.h
@@ -6,8 +6,22 @@
 #define ROBOT_CODE_BINDING_H
 
 
-class Binding {
+#include <functional>
+#include "okapi/api.hpp"
 
+typedef std::function<void (void)> CallbackFn;
+
+class Binding {
+public:
+    Binding(const okapi::ControllerButton & button, CallbackFn callback);
+    Binding(okapi::ControllerButton button, CallbackFn callback, int timeout);
+
+    void update();
+private:
+    okapi::ControllerButton m_button;
+    const CallbackFn m_callback;
+    const int m_timeout;
+    int m_timeout_count;
 };
 
 
--- src/util/Binding.cpp
+++ src/util/Binding.cpp
@@ -2,4 +2,24 @@
 // Created by ariel on 11/28/19.
 //
 
-#include "Binding.h"
+#include "smc/util/Binding.h"
+
+#include <utility>
+
+
+Binding::Binding(const okapi::ControllerButton &bind, CallbackFn callback) :
+    Binding(bind, callback, 0) {}
+
+Binding::Binding(okapi::ControllerButton bind, CallbackFn callback, int timeout) :
+        m_button(std::move(bind)), m_callback(std::move(callback)), m_timeout(timeout) {
+    m_timeout_count = 0;
+}
+
+void Binding::update() {
+    if (m_button.isPressed() && m_timeout_count == 0) {
+        m_callback();
+        m_timeout_count = m_timeout;
+    }
+
+    if (m_timeout_count > 0) m_timeout_count--;
+}
