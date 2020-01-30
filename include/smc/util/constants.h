05c7265472f4a5158de7cfc0b34e355b61c12226
Ariel Young <arielyoung103@gmail.com>
2020-01-29 16:37:13 -0800

    Finalized ports for competition

--- include/smc/util/constants.h
+++ include/smc/util/constants.h
@@ -10,31 +10,31 @@
 using namespace okapi;
 namespace robot {
     /* Begin drive motor block */
-    const int FRONT_LEFT_DRIVE_MOTOR_PORT = 16;
-    const int FRONT_RIGHT_DRIVE_MOTOR_PORT = -15;
-    const int BACK_LEFT_DRIVE_MOTOR_PORT = 12;
-    const int BACK_RIGHT_DRIVE_MOTOR_PORT = -11;
+    const int FRONT_LEFT_DRIVE_MOTOR_PORT = 0;
+    const int FRONT_RIGHT_DRIVE_MOTOR_PORT = 0;
+    const int BACK_LEFT_DRIVE_MOTOR_PORT = 13;
+    const int BACK_RIGHT_DRIVE_MOTOR_PORT = 2;
     /* End drive motor block */
 
 
     /* Begin intake block */
-    const int INTAKE_MOTOR_PORT_LEFT = -1; // TODO: Placeholder value
-    const int INTAKE_MOTOR_PORT_RIGHT = 10; // TODO: Placeholder value
-    const int INTAKE_POS_MOTOR_PORT_LEFT = -9;
-    const int INTAKE_POS_MOTOR_PORT_RIGHT = 2;
+    const int INTAKE_MOTOR_PORT_LEFT = -11; // TODO: Placeholder value
+    const int INTAKE_MOTOR_PORT_RIGHT = 1; // TODO: Placeholder value
+    const int INTAKE_POS_MOTOR_PORT_LEFT = -12;
+    const int INTAKE_POS_MOTOR_PORT_RIGHT = 3;
 
     const char INTAKE_POS_LIMIT_SWITCH_UP = 'A'; // TODO: Placeholder value
     const char INTAKE_POS_LIMIT_SWITCH_DOWN = 'A'; //TODO: Placeholder value
 
     const float INTAKE_DOWN_POS = 0;
-    const float INTAKE_UP_POS_LEFT = 410.0;
-    const float INTAKE_UP_POS_RIGHT = 410.0;
+    const float INTAKE_UP_POS_LEFT = 910.0;
+    const float INTAKE_UP_POS_RIGHT = 910.0;
     /* End intake block */
 
 
     /* Begin tray block */
     const int TRAY_POS_MOTOR_PORT = 15;
-    const int TRAY_MOTOR_POS_UP = 410; // TODO: Placeholder value
+    const int TRAY_MOTOR_POS_UP = 140; // TODO: Placeholder value
 
     const char TRAY_POS_DOWN_LIMIT_SWITCH = 'H';
     /* End tray block */
