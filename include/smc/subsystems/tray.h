328d068398cdcb1e0081a55c5078b6c2671955d1
Ariel Young <arielyoung103@gmail.com>
2020-01-29 16:00:53 -0800

    Began writing tray subsystem

--- include/smc/subsystems/tray.h
+++ include/smc/subsystems/tray.h
@@ -5,10 +5,28 @@
 #ifndef ROBOT_CODE_TRAY_H
 #define ROBOT_CODE_TRAY_H
 
+#include "api.h"
+#include "okapi/api.hpp"
+
 namespace tray {
+    enum TrayPosition { UP = 1, DOWN = 0 };
+    extern TrayPosition current_pos;
+
+    void init();
+    void update();
+
+    void moveTrayToPosition(TrayPosition pos);
+    void togglePosition();
+    void placeStack();
+
     const int trayPosKP = 1; // TODO: Tune these PID values as well
     const int trayPosKI = 1;
     const int trayPosKD = 1;
+
+    extern int limit_timeout;
+
+    extern std::unique_ptr<okapi::ADIButton> tray_limit_switch;
+    extern std::unique_ptr<okapi::Motor> tray_position_motor;
 }
 
 #endif //ROBOT_CODE_TRAY_H
--- src/subsystems/tray.cpp
+++ src/subsystems/tray.cpp
@@ -1,8 +1,48 @@
 //
 // Created by ariel on 11/19/19.
 //
+#include "smc/util/util.h"
+#include "smc/robot.h"
 #include "smc/subsystems/tray.h"
 
 namespace tray {
-    
+    int limit_timeout;
+    TrayPosition current_pos;
+
+    std::unique_ptr<okapi::ADIButton> tray_limit_switch;
+    std::unique_ptr<okapi::Motor> tray_position_motor;
+
+
+    void init() {
+        tray_position_motor = util::initMotor(robot::TRAY_POS_MOTOR_PORT);
+        tray_limit_switch = util::initLimitSwitch(robot::TRAY_POS_DOWN_LIMIT_SWITCH);
+
+        tray_position_motor->tarePosition();
+        current_pos = DOWN;
+    }
+
+    void update() {
+        if (tray_limit_switch->isPressed() && limit_timeout == 0) {
+            tray_position_motor->tarePosition();
+            tray_position_motor->moveAbsolute(0, 1);
+            current_pos = DOWN;
+        }
+        else if (limit_timeout > 0)
+            limit_timeout--;
+    }
+
+    void moveTrayToPosition(TrayPosition pos) {
+        if (pos == UP) {
+            tray_position_motor->moveAbsolute(robot::TRAY_MOTOR_POS_UP, 120);
+            limit_timeout = 40;
+            current_pos = UP;
+        } else if (pos == DOWN) {
+            tray_position_motor->moveAbsolute(0, 120);
+            current_pos = DOWN;
+        }
+    }
+
+    void togglePosition() {
+        moveTrayToPosition((TrayPosition) !current_pos);
+    }
 }
