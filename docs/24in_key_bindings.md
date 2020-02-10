697781c327d4da3738b4e40e1f0cde92d34c8fa7
Nashir Janmohamed <nashirbass@gmail.com>
2020-02-10 14:54:22 -0800

    Added a better image for the key binding script

--- docs/24in_key_bindings.md
+++ docs/24in_key_bindings.md
@@ -1,8 +1,14 @@
 # Key bindings for SMC 2019-2020 24in robot
-![Controller](https://raw.githubusercontent.com/smcrobotics/competition_bot_15_inch/master/docs/controller.JPG)
+![Controller](https://raw.githubusercontent.com/smcrobotics/competition_bot_15_inch/master/docs/controller.png)
 
 ## Button mappings
-### Arrows (1-4), Letters (5-8), Bumpers (9-12)
+### Bumpers (1-4), Joysticks (5-6), Arrows (7-10), Letters (11-14)
+1. R1: intake button 
+1. L1: outtake button
+1. R2: N/A
+1. L2: N/A
+1. Left Joystick: Left side drive motors
+1. Right Joystick: Right side drive motors
 1. down: intake pos down
 1. left: raise tray
 1. up: intake pos up
@@ -11,7 +17,3 @@
 1. X: N/A
 1. A: place stack
 1. B: drive brake toggle
-1. R1: intake button 
-1. L1: outtake button
-1. R2: N/A
-1. L2: N/A
--- parse_key_bindings_24in.py
+++ parse_key_bindings_24in.py
@@ -27,9 +27,21 @@ source.close()
 
 with open('./docs/24in_key_bindings.md', 'a') as output_file:
     output_file.write("# Key bindings for SMC 2019-2020 24in robot\n")
-    output_file.write("![Controller](https://raw.githubusercontent.com/smcrobotics/competition_bot_15_inch/master/docs/controller.JPG)\n")
+    output_file.write("![Controller](https://raw.githubusercontent.com/smcrobotics/competition_bot_15_inch/master/docs/controller.png)\n")
     output_file.write("\n## Button mappings\n")
-    output_file.write("### Arrows (1-4), Letters (5-8), Bumpers (9-12)\n")
+    output_file.write("### Bumpers (1-4), Joysticks (5-6), Arrows (7-10), Letters (11-14)\n")
+
+    for button in bumpers:
+        output_file.write("1. {}: ".format(button))
+        if button in bindings:
+            output_file.write("{}".format(bindings[button]))
+        else:
+            output_file.write("N/A")
+        output_file.write("\n")
+
+    output_file.write("1. Left Joystick: Left side drive motors\n")
+    output_file.write("1. Right Joystick: Right side drive motors\n")
+
     for button in arrows:
         output_file.write("1. {}: ".format(button))
         if button in bindings:
@@ -39,14 +51,6 @@ with open('./docs/24in_key_bindings.md', 'a') as output_file:
         output_file.write("\n")
     
     for button in letters:
-        output_file.write("1. {}: ".format(button))
-        if button in bindings:
-            output_file.write("{}".format(bindings[button]))
-        else:
-            output_file.write("N/A")
-        output_file.write("\n")
-
-    for button in bumpers:
         output_file.write("1. {}: ".format(button))
         if button in bindings:
             output_file.write("{}".format(bindings[button]))
