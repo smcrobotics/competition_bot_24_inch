4df097a6ceae044972ad67f7122d41174fb21bc9
Ariel Young <arielyoung103@gmail.com>
2020-02-27 15:56:56 -0800

    Transitioned intake to singleton class pattern rather than gross namespace hack

--- include/smc/subsystems/Intake.h
+++ include/smc/subsystems/Intake.h
@@ -6,45 +6,25 @@
 #define ROBOT_CODE_INTAKE_H
 
 #include "api.h"
+#include "smc/subsystems/AbstractSubsystem.h"
 #include "okapi/api.hpp"
 
-namespace intake {
-    enum IntakePosition { UP, DOWN };
-
-    void init();
-
-    void update();
-    void printPos();
-
-    void moveArmsToPosition(IntakePosition position);
-
-    void setIntakeVelocity(int percent);
-
-    const int intakePosKP = 1; // TODO: Tune this PID loop
-    const int intakePosKI = 1;
-    const int intakePosKD = 1;
-
-    extern std::unique_ptr<okapi::Motor> left_intake_motor;
-    extern std::unique_ptr<okapi::Motor> right_intake_motor;
-    extern std::unique_ptr<okapi::Motor> right_intake_pos_motor;
-    extern std::unique_ptr<okapi::Motor> left_intake_pos_motor;
-}
-
 namespace subsystems {
-    class Intake {
+class Intake : public Subsystems::AbstractSubsystem {
     public:
-        Intake();
-        void update();
-        void printDebug();
+        static Intake & getInstance();
+        Intake(const Intake &) = delete;
+        void operator=(const Intake & lhs) = delete;
+
+        void update() override;
+        void printDebug() override;
         void setIntakeVelocity(int percent);
 
-        void intakeMax(); // These methods are for controller bindings, should never be used in code
-        void intakeMin();
-        void intakeZero();
-
     private:
-        int current_intake_percent;
+        Intake();
 
+        float current_intake_voltage;
+        int current_intake_percent;
         std::unique_ptr<okapi::Motor> left_intake_motor;
         std::unique_ptr<okapi::Motor> right_intake_motor;
     };
--- src/subsystems/Intake.cpp
+++ src/subsystems/Intake.cpp
@@ -4,73 +4,37 @@
 #include <iostream>
 
 #include "smc/util/util.h"
-#include "smc/subsystems/intake.h"
+#include "smc/subsystems/Intake.h"
 #include "smc/util/constants.h"
 
 using std::cout;
 using std::endl;
-
-namespace intake {
-    std::unique_ptr<okapi::Motor> left_intake_motor;
-    std::unique_ptr<okapi::Motor> right_intake_motor;
-    std::unique_ptr<okapi::Motor> right_intake_pos_motor;
-    std::unique_ptr<okapi::Motor> left_intake_pos_motor;
-
-    void init() {
-        left_intake_motor = util::initMotor(robot::INTAKE_MOTOR_PORT_LEFT, okapi::AbstractMotor::gearset::blue);
-        right_intake_motor = util::initMotor(robot::INTAKE_MOTOR_PORT_RIGHT, okapi::AbstractMotor::gearset::blue);
-        left_intake_pos_motor = util::initMotor(robot::INTAKE_POS_MOTOR_PORT_LEFT, okapi::AbstractMotor::gearset::blue);
-        right_intake_pos_motor = util::initMotor(robot::INTAKE_POS_MOTOR_PORT_RIGHT, okapi::AbstractMotor::gearset::blue);
-
-
-        left_intake_pos_motor->tarePosition();
-        right_intake_pos_motor->tarePosition();
-    }
-
-    void update() {
-
-    }
-
-    void moveArmsToPosition(intake::IntakePosition position) {
-        if (position == UP) {
-            right_intake_pos_motor->moveAbsolute(robot::INTAKE_UP_POS_RIGHT, 120);
-            left_intake_pos_motor->moveAbsolute(robot::INTAKE_UP_POS_LEFT, 120);
-        } else if (position == DOWN) {
-            right_intake_pos_motor->moveAbsolute(robot::INTAKE_DOWN_POS, 80);
-            left_intake_pos_motor->moveAbsolute(robot::INTAKE_DOWN_POS, 80);
-        }
-    }
-
-    void printPos() {
-        std::cout << "Left intake_pos motor position: " << left_intake_pos_motor->getPosition() << std::endl;
-        std::cout << "Right intake_pos motor position: " << right_intake_pos_motor->getPosition() << std::endl;
-    }
-
-    void setIntakeVelocity(int percent) {
-        float voltage = (percent / 100.0f) * 12000;
-        left_intake_motor->moveVoltage((int) voltage);
-        right_intake_motor->moveVoltage((int) voltage);
-    }
-}
 
 namespace subsystems {
     Intake::Intake() {
         left_intake_motor = util::initMotor(robot::INTAKE_MOTOR_PORT_LEFT, okapi::AbstractMotor::gearset::blue);
         right_intake_motor = util::initMotor(robot::INTAKE_MOTOR_PORT_RIGHT, okapi::AbstractMotor::gearset::blue);
         current_intake_percent = 0;
+        current_intake_voltage = 0;
+    }
+
+    Intake & Intake::getInstance() {
+        static Intake intake;
+        return intake;
     }
 
     void Intake::update() {
-        float voltage = (current_intake_percent / 100.0f) * 12000;
-        left_intake_motor->moveVelocity(voltage);
-        right_intake_motor->moveVelocity(voltage);
+        left_intake_motor->moveVelocity(current_intake_voltage);
+        right_intake_motor->moveVelocity(current_intake_voltage);
     }
 
     void Intake::setIntakeVelocity(int percent) {
+        current_intake_voltage = ((float) percent / 100.0f) * 12000;
         current_intake_percent = percent;
     }
 
     void Intake::printDebug() {
-        cout << "[DEBUG][Intake] Intake velocity: "
+        cout << "[DEBUG][Intake] Intake velocity: " << current_intake_percent << endl;
+        cout << "[DEBUG][Intake] Intake voltage: " << current_intake_voltage << endl;
     }
 }
