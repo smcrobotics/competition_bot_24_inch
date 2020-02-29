27a46c9ff2a0a816d49a6b9827f145764704323f
Ariel Young <arielyoung103@gmail.com>
2020-02-28 16:15:42 -0800

    Removed namespace hack, switched to singleton for Tray

--- include/smc/subsystems/Tray.h
+++ include/smc/subsystems/Tray.h
@@ -9,30 +9,6 @@
 #include "okapi/api.hpp"
 #include "smc/subsystems/AbstractSubsystem.h"
 
-namespace tray {
-    enum TrayPosition { UP = 1, DOWN = 0 };
-    extern TrayPosition current_pos;
-
-    void init();
-    void update();
-    void printPos();
-
-    void moveTrayToPosition(TrayPosition pos, bool blocking);
-    void moveTrayToPosition(TrayPosition pos);
-
-    void setTrayVelocity(int percent);
-    void togglePosition();
-
-    const int trayPosKP = 1; // TODO: Tune these PID values as well
-    const int trayPosKI = 1;
-    const int trayPosKD = 1;
-
-    extern int limit_timeout;
-
-    extern std::unique_ptr<okapi::ADIButton> tray_limit_switch;
-    extern std::unique_ptr<okapi::Motor> tray_position_motor;
-}
-
 namespace subsystems {
 class Tray : public AbstractSubsystem {
     public:
@@ -48,6 +24,8 @@ class Tray : public AbstractSubsystem {
 
         void moveTrayToPosition(TrayPosition pos, bool blocking);
         void moveTrayToPosition(TrayPosition pos);
+
+        void trayMoveManual(int motorSpeedPercent);
 
         static void togglePosition();
 
--- src/subsystems/Tray.cpp
+++ src/subsystems/Tray.cpp
@@ -8,69 +8,6 @@
 using std::cout;
 using std::endl;
 
-namespace tray {
-    int limit_timeout;
-    TrayPosition current_pos;
-
-    std::unique_ptr<okapi::ADIButton> tray_limit_switch;
-    std::unique_ptr<okapi::Motor> tray_position_motor;
-
-
-    void init() {
-        tray_position_motor = util::initMotor(robot::TRAY_POS_MOTOR_PORT);
-        tray_limit_switch = util::initLimitSwitch(robot::TRAY_POS_DOWN_LIMIT_SWITCH);
-
-        tray_position_motor->tarePosition();
-        current_pos = DOWN;
-    }
-
-    void update() {
-        if (tray_limit_switch->isPressed() && limit_timeout == 0 && tray_position_motor->getActualVelocity() <= 0) {
-            tray_position_motor->tarePosition();
-            tray_position_motor->moveAbsolute(0, 1);
-            current_pos = DOWN;
-            limit_timeout = 50;
-            cout << "tar boi" << endl;
-        }
-        else if (limit_timeout > 0)
-            limit_timeout--;
-    }
-
-    void printPos() {
-        cout << "[POS] Tray motor position: " << tray_position_motor->getPosition() << endl;
-    }
-
-    void moveTrayToPosition(TrayPosition pos, bool blocking) {
-        // int motor_velocity = 40;
-        if (pos == UP) {
-            tray_position_motor->moveAbsolute(robot::TRAY_MOTOR_POS_UP, 40); // TODO: change to constants::AUTO_TRAY_SPEED
-            limit_timeout = 200;
-            current_pos = UP;
-        } else if (pos == DOWN) {
-            tray_position_motor->moveAbsolute(0, 40); // TODO: change to constants::AUTO_TRAY_SPEED
-            current_pos = DOWN;
-        }
-
-        if (blocking) { // choose whether to block other tasks from performing
-            while (!tray_position_motor->isStopped())
-                pros::delay(10);
-        }
-    }
-
-    void moveTrayToPosition(TrayPosition pos) {
-        moveTrayToPosition(pos, false);
-    }
-
-    void setTrayVelocity(int percent) {
-        float voltage = ((float) percent / 100.0f) * 12000;
-        tray_position_motor->moveVoltage((int) voltage);
-    }
-
-    void togglePosition() {
-        moveTrayToPosition((TrayPosition) !current_pos, false);
-    }
-}
-
 namespace subsystems {
     Tray::Tray() : limit_timeout(0), current_pos(DOWN), did_tare(false) {
         tray_position_motor = util::initMotor(robot::TRAY_POS_MOTOR_PORT);
@@ -133,6 +70,10 @@ namespace subsystems {
 
     void Tray::moveTrayToPosition(TrayPosition pos) {
         moveTrayToPosition(pos, false);
+    }
+
+    void Tray::trayMoveManual(int motorSpeedPercent) {
+        tray_position_motor->moveVoltage((int) ((float) motorSpeedPercent / 100.0f) * 12000);
     }
 
     void Tray::togglePosition() {
